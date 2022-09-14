d6c38ba31fe5422064df34fa9eb73c5d7e0335b1
Weector <108277280+Weector@users.noreply.github.com>
2022-09-14 21:09:40 +0000

    Deploying to gh-pages from @ Weector/goit-js-hw-09@83655f2c0cb65c54b5b76c2924684b376318e76e 🚀

---
+++ 01-color-switcher.7843bc74.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function r(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(e){d.style.backgroundColor=r(),timerId=setInterval((()=>{d.style.backgroundColor=r()}),1e3),e.target.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(t){clearInterval(timerId),t.target.disabled=!0,e.disabled=!1})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.7843bc74.js.map
